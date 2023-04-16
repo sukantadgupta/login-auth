de10e301bb875979f591ca1df42b7ac30b4df1a7
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-04-16 11:08:19 +0530

    Signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Switch, Route } from 'react-router-dom';
 
-import './App.css';
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
- hi
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/profile'>
+          <UserProfile />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
